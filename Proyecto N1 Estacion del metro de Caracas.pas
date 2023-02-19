58ddee92830eaac927d9fabb8b970f1d68702e9e
angelperezunimar <aperez.9625@unimar.edu.ve>
2023-02-19 19:07:53 -0400

    Mejora en la identacion y añadi la opcion de seleccion para boleto en metro o en metrobus y/o cantidad de viajes en la seleccion de boletos

--- Proyecto N1 Estacion del metro de Caracas.pas
+++ Proyecto N1 Estacion del metro de Caracas.pas
@@ -84,109 +84,137 @@ begin
 					else 
 		
 
-        case boletos of {sistema de compra de boletos}
-		'a': begin 
-		writeln('El boleto simple tiene una cobertura de: 1 viaje en metro');
-		writeln('cuantos boletos quiere comprar?');
-		readln (c);
-		precio:= c*b;
-			if c>0 then
-				writeln('su coste es de:',precio,'-dolares')
-				else
-		        writeln ('hasta luego, tenga feliz dia.');
-	     end;
-		'b': begin
-		 writeln('El boleto integrado tiene una cobertura de: 1 viaje en metro y 1 viaje en metrobus');
-		 writeln ('cuantos boletos quiere comprar?');
-		 readln (d);
-		 precio2:=d*p2;
-			if d>0 then
-			 writeln ('su coste es de:',precio2,'-dolares')
-			 else
-			 writeln ('hasta luego, tenga feliz dia.');
-		 end;
-		'c':begin
-		writeln('El boleto ida y vuelta tiene una cobertura de: 2 viajes en metro');
-		writeln ('cuantos boletos quiere comprar?');
-		 readln (e);
-		 precio2:=e*p3;
-			if e>0 then
-			 writeln ('su coste es de:',precio3,'-dolares')
-			 else
-			 writeln ('hasta luego, tenga feliz dia.');
-		end;
-		'd':begin 
-		writeln ('el boleto ida y vuelta integrado tiene una cobertura de: 2 viajes en metro y 2 viajes en metrobus');
-		writeln ('cuantos boletos quiere comprar?');
-		 readln (f);
-		 precio3:=f*p4;
-			if f>0 then
-			 writeln ('su coste es de:',precio3,'-dolares')
-			 else
-			 writeln ('hasta luego, tenga feliz dia.');
-		end;
-		'e':begin
-		writeln('El boleto multiabono tiene una cobertura de: 10 viajes en metro');
-		writeln ('cuantos boletos desea comprar?');
-			readln (g);
-			precio4:=g*p5;
-			if g>0 then
-			writeln ('su coste es de:',precio4,'-dolares')
-			else
-			writeln('hasta luego, tenga feliz dia.');
-		
-		end;
-		'f':begin
-		writeln('El boleto multiabono integrado tiene una cobertura de: 10 viajes en metro y 10 viajes en metrobus');
-		writeln('cuantos boletos desea comprar?');
-		readln (h);
-		precio5:=h*p6;
-		if h>0 then
-		writeln ('su coste es de:',precio5,'-dolares')
-		else
-		writeln ('hasta luego, tenga feliz dia.');
-		
-		end;
-		'g':begin
-		writeln('El boleto estudiantil simple tiene una cobertura de: 10 viajes en metro');
-		writeln('cuantos boletos quiere comprar?');
-		readln(i);
-		precio6:=i*p7;
-		if i>0 then
-		writeln ('su coste es de:',precio6,'-dolares')
-		else
-		writeln('hasta luego, tenga feliz dia.');
-		end;
-		'h':begin
-		writeln('El boleto estudiantil integrado tiene una cobertura de: 10 viajes en metro y 20 viajes en metrobus');
-		writeln('cuantos boletos quiere comprar?');
-		readln(j);
-		precio7:=j*p8;
-		if j>0 then
-		writeln ('su coste es de:',precio7,'-dolares')
-		else
-		writeln('hasta luego, tenga feliz dia');
-		end;
-		'i':begin
-		writeln('El boleto metroTarjeta tiene una cobertura de: 20, 30, y 40 viajes');
-		writeln('cuantos boletos quiere comprar?');
-		readln(k);
-		precio8:=k*p9;
-		if k>0 then
-		writeln('su coste es de:',precio8,'-dolares')
-		else
-		writeln('hasta luego, tenga feliz dia');
-		end;
-		'j':begin
-		writeln('El boleto metroTarjeta integrado tiene una cobertura de: 20,30 y 40 viajes en metro ademas de metrobus');
-		writeln ('cuantos boletos quiere comprar?');
-		readln(l);
-		precio9:=l*p10;
-		if l>0 then
-		writeln ('su coste es de:',precio9,'-dolares')
-		else
-		writeln('hasta luego tenga feliz dia'); {Despedida en caso de no querer boletos}
-		end;
+        case boletos of
+					'a': begin 
+						writeln('El boleto simple tiene una cobertura de: 1 viaje en metro');
+						writeln('cuantos boletos quiere comprar?');
+						readln (c);
+						precio:= c*b;
+						if c>0 then
+						writeln('su coste es de:',precio,'-dolares')
+						else
+						writeln ('hasta luego, tenga feliz dia.');
+						end;
+					'b': begin
+						writeln('El boleto integrado tiene una cobertura de: 1 viaje en metro o 1 viaje en metrobus');
+						writeln('¿Que desea hacer?');
+						writeln('a. 1 Viaje en metro');
+						writeln('b. 1 Viaje en metrobus');
+						readln(tipodeviaje);
+						writeln ('cuantos boletos quiere comprar?');
+						readln (d);
+						precio2:=d*p2;
+						if d>0 then
+						writeln ('su coste es de:',precio2,'-dolares')
+						else
+						writeln ('hasta luego, tenga feliz dia.');
+						end;
+					'c':begin
+						writeln('El boleto ida y vuelta tiene una cobertura de: 2 viajes en metro');
+						writeln ('cuantos boletos quiere comprar?');
+						readln (e);
+						precio2:=e*p3;
+						if e>0 then
+						writeln ('su coste es de:',precio3,'-dolares')
+						else
+						writeln ('hasta luego, tenga feliz dia.');
+						end;
+					'd':begin 
+						writeln ('el boleto ida y vuelta integrado tiene una cobertura de: 2 viajes en metro o 2 viajes en metrobus');
+						writeln('¿Que desea hacer?');
+						writeln('a. 2 Viajes en metro');
+						writeln('b. 2 Viajes en metrobus');
+						readln(tipodeviaje);
+						writeln ('cuantos boletos quiere comprar?');
+						readln (f);
+						precio3:=f*p4;
+						if f>0 then
+						writeln ('su coste es de:',precio3,'-dolares')
+						else
+						writeln ('hasta luego, tenga feliz dia.');
+						end;
+					'e':begin
+						writeln('El boleto multiabono tiene una cobertura de: 10 viajes en metro');
+						writeln ('cuantos boletos desea comprar?');
+						readln (g);
+						precio4:=g*p5;
+						if g>0 then
+						writeln ('su coste es de:',precio4,'-dolares')
+						else
+						writeln('hasta luego, tenga feliz dia.');
+				
+						end;
+					'f':begin
+						writeln('El boleto multiabono integrado tiene una cobertura de: 10 viajes en metro o 10 viajes en metrobus');
+						writeln('¿Que desea hacer?');
+						writeln('a. 10 Viajes en metro');
+						writeln('b. 10 Viajes en metrobus');
+						readln(tipodeviaje);
+						writeln('cuantos boletos desea comprar?');
+						readln (h);
+						precio5:=h*p6;
+						if h>0 then
+						writeln ('su coste es de:',precio5,'-dolares')
+						else
+						writeln ('hasta luego, tenga feliz dia.');
+				
+						end;
+					'g':begin
+						writeln('El boleto estudiantil simple tiene una cobertura de: 10 viajes en metro');
+						writeln('cuantos boletos quiere comprar?');
+						readln(i);
+						precio6:=i*p7;
+						if i>0 then
+						writeln ('su coste es de:',precio6,'-dolares')
+						else
+						writeln('hasta luego, tenga feliz dia.');
+						end;
+					'h':begin
+						writeln('El boleto estudiantil integrado tiene una cobertura de: 10 viajes en metro o 20 viajes en metrobus');
+						writeln('¿Que desea hacer?');
+						writeln('a. 10 Viajes en metro');
+						writeln('b. 20 Viajes en metrobus');
+						readln(tipodeviaje);
+						writeln('cuantos boletos quiere comprar?');
+						readln(j);
+						precio7:=j*p8;
+						if j>0 then
+						writeln ('su coste es de:',precio7,'-dolares')
+						else
+						writeln('hasta luego, tenga feliz dia');
+						end;
+					'i':begin
+						writeln('El boleto metroTarjeta tiene una cobertura de: 20, 30, o 40 viajes');
+						writeln('¿Que desea hacer?');
+						writeln('a. 20 Viajes en metro');
+						writeln('b. 30 Viajes en metro');
+						writeln('c. 40 Viajes en metro');
+						readln(tipodeviaje);
+						writeln('cuantos boletos quiere comprar?');
+						readln(k);
+						precio8:=k*p9;
+						if k>0 then
+						writeln('su coste es de:',precio8,'-dolares')
+						else
+						writeln('hasta luego, tenga feliz dia');
+						end;
+					'j':begin
+						writeln('El boleto metroTarjeta integrado tiene una cobertura de: 20,30 y 40 viajes en metro ademas de metrobus');
+						writeln('a. 20 Viajes en metro');
+						writeln('b. 30 Viajes en metro');
+						writeln('c. 40 Viajes en metro');
+						writeln('d. 20 Viajes en metrobus');
+						writeln('e. 30 Viajes en metrobus');
+						writeln('f. 40 Viajes en metrobus');
+						readln(tipodeviaje);
+						writeln ('cuantos boletos quiere comprar?');
+						readln(l);
+						precio9:=l*p10;
+						if l>0 then
+						writeln ('su coste es de:',precio9,'-dolares')
+						else
+						writeln('hasta luego tenga feliz dia'); {:D}
+						end;
 	end;
 	
 	
